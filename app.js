ae2b66688077cb7cb77e5fafa43a61f116094546
EC2 Default User <ec2-user@ip-172-31-16-206.us-east-2.compute.internal>
2020-03-23 04:58:23 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res) {
+    res.render("venus.html");
+});
+
+app.get("/earth", function(req, res) {
+    res.render("earth.html");
+});
+
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+
+//server listener
+app.listen("8081", "127.0.0.1", function(){
+    console.log("Express server is Running...");
+});
+
